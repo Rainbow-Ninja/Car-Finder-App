c2bacafcc01d079954712df1fe5bf4f54fde00fe
Rainbow-Ninja <scaffelpike@yahoo.com.au>
2019-07-02 12:36:40 +1000

    add profile creation, listing cars and image upload feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    # before_action :authenticate_user!
+    def after_sign_in_path_for(profile)
+        if current_user.profile
+            new_car_path
+        else
+            new_profile_path || root_path
+        end
+    end
 end
---
+++ app/models/car.rb
@@ -0,0 +1,5 @@
+class Car < ApplicationRecord
+    belongs_to :seller
+    #associate to the active storage images
+    has_many_attached :pictures
+end
